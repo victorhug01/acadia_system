87e908c9841b4d26b833e1a5ff609d49d0931c95
victor <victorhugobbpereira02@gmail.com>
2024-09-17 21:04:35 -0300

    finalização design login

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
-import 'dart:io';
-
 import 'package:acadia/src/app/myapp.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows) {
-    // Inicializa a janela do Bitsdojo
-    doWhenWindowReady(() {
-      final window = appWindow;
-      window.maximize();
-      window.show();
-    });
-  }
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = const WindowOptions(
+    minimumSize: Size(800, 600),
+    alwaysOnTop: true,
+    center: true,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+    windowButtonVisibility: true,
+    title: ''
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   await Supabase.initialize(
     url: 'https://awamsrvpvsysyhflmvei.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3YW1zcnZwdnN5c3loZmxtdmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2NTM5MjQsImV4cCI6MjAzOTIyOTkyNH0.-VaduMrkDitGXYhxYzVZSe51qL1Nr1Prt6bmvlcXOEw',
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,8 +4,9 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
-  bitsdojo_window_windows
+  screen_retriever
   url_launcher_windows
+  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
