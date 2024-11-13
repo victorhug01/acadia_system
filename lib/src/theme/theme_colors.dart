bc11df3106d7bb283ba9e824134b1dfc8a5d7900
Victor Hugo <victorhugobbpereira02@gmail.com>
2024-11-13 11:14:35 -0300

    cores corretas

--- lib/src/theme/theme_colors.dart
+++ lib/src/theme/theme_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class ColorSchemeManagerClass{
   static Color colorPrimary = const Color(0xff4A007B);
-  static Color colorSecondary = const Color.fromARGB(255, 232, 214, 245);
+  static Color colorSecondary = const Color.fromARGB(255, 242, 237, 246);
   static Color colorWhite = const Color(0xffffffff);
   static Color colorBlack = const Color(0xff000000);
   static Color colorDanger = const Color.fromARGB(255, 210, 50, 50);
